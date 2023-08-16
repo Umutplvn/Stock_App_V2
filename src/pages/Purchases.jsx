9cd90f6cd32eb338d1d61956ddc197fd5b43232d
umutplvn <umutpehlivan2078@gmail.com>
2023-08-16 04:11:08 +0100

    Purchases Page

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import useStockData from '../hooks/useStockData'
 
-const Purchases = () => {
+
+const Purchases = () => {   
+  
+const {purchases} =useSelector((state)=>state.stock)
+const {getStockData} = useStockData()
+
+
+useEffect(() => {
+  getStockData("purchases")
+}, [])
+
+
+console.log(purchases);
   return (
     <div>Purchases</div>
   )
