f93d8360e73093fbf3e90e8c7beb6ff433edcd4d
umutplvn <umutpehlivan2078@gmail.com>
2023-08-16 04:12:09 +0100

    Global Styles and Private Router compiled

--- src/pages/PrivateRouter.jsx
+++ src/pages/PrivateRouter.jsx
@@ -4,7 +4,8 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRouter = () => {
     const {currentUser}=useSelector((state)=>state.auth)
-  return( currentUser ? <Outlet/> : <Navigate to="/"/> )
+  return (currentUser ? <Outlet/> : <Navigate to="/"/> )
+    
 }
 
 export default PrivateRouter
