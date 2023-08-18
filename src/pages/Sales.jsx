b92f49acf6280d5d6468a276a5df7fe8bd613824
umutplvn <umutpehlivan2078@gmail.com>
2023-08-18 02:28:39 +0100

    Sales Page Drafts

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import {useEffect, useState} from "react";
+import ProductTable from "../components/ProductTable";
+import ProductModal from "../components/ProductModal";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import useStockData from "../hooks/useStockData";
+import { useSelector } from "react-redux";
+
 
 const Sales = () => {
-  return (
-    <div>Sales</div>
-  )
-}
+  const {getStockData}= useStockData()
+  const { sales } = useSelector((state) => state.stock)
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-export default Sales
+  useEffect(() => {
+getStockData("sales")
+getStockData("categories")
+getStockData("brands")
+
+  }, [])
+
+
+  return (
+    <div>
+        <Typography color={"error"} component={"h3"} variant="h5">
+        Sales
+      </Typography>
+
+      <Button variant="contained" onClick={handleOpen} sx={{ mt: "10px" }} >
+        New Sale
+      </Button>
+{/* 
+      <ProductModal open={open} handleClose={handleClose}/>
+      <ProductTable /> */}
+    </div>
+  );
+};
+
+export default Sales;
